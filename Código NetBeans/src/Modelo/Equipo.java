5e739f6b052ef623784cc039cca34520a6055675
Rystron2007 <79596829+Rystron2007@users.noreply.github.com>
2021-06-30 11:00:17 -0500

    Actualización de Equipo

--- Código NetBeans/src/Modelo/Equipo.java
+++ Código NetBeans/src/Modelo/Equipo.java
@@ -3,11 +3,10 @@ package Modelo;
 import java.util.Date;
 
 public class Equipo {
+
     //Atributos pertenecientes a un Equipo
     private String nombreEquipo;
     private String paisOrigen;
-    //Contrato Afiliado con un DirectorTécnico
-    private Contrato contrato;
 
     //Constructor Parametrizado
     public Equipo(String nombreEquipo, String paisOrigen) {
@@ -21,26 +20,21 @@ public class Equipo {
         Date fechaInicio = new Date();
         Date fechaFin = new Date();
         Estado estado = Estado.PENDIENTE;
-        
+
         //Creación de Prueba para el Director Técnico
         DirectorTecnico directorTecnico = new DirectorTecnico("cedula",
-                new ActaNacimiento("01234","John", "Palacios", "Ecuatoriano"), 20, "Sauces", "john@gmail.com", 10);
+                new ActaNacimiento("01234", "John", "Palacios", "Ecuatoriano"), 20, "Sauces", "john@gmail.com", 10);
 
         Contrato contrato = new Contrato(codigoContrato, fechaInicio, fechaFin, estado, directorTecnico);
 
-        this.contrato = contrato;
         return contrato;
     }
-    
-    public void eliminarContrato(){
+
+    public void eliminarContrato() {
         //Se válida el Contrato existente y se procede con su Eliminación
     }
 
     //Métodos GET y SET
-    public void setContrato() {
-        this.contrato = crearContrato();
-    }
-
     public String getNombreEquipo() {
         return nombreEquipo;
     }
@@ -55,13 +49,5 @@ public class Equipo {
 
     public void setPaisOrigen(String paisOrigen) {
         this.paisOrigen = paisOrigen;
-    }
-
-    public Contrato getContrato() {
-        return contrato;
-    }
-
-    public void setContrato(Contrato contrato) {
-        this.contrato = contrato;
     }
 }
--- Código NetBeans/src/Modelo/EquipoDB.java
+++ Código NetBeans/src/Modelo/EquipoDB.java
@@ -3,30 +3,32 @@ package Modelo;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
-public class ArbitroDB implements CRUD {
+public class EquipoDB implements CRUD {
 
     Connection conector;
     PreparedStatement ps;
     ResultSet rs;
 
+    //Solucionar Problemas de ID
     @Override
     public void registrar(String query, Object objeto) {
-        Arbitro arbitro = (Arbitro) objeto;
+        Equipo equipo = (Equipo) objeto;
         try {
             conector = Conexion.getConnection();
             ps = conector.prepareStatement(query);
-            ps.setString(1, arbitro.getCedula());
-            ps.setString(2, arbitro.getActaNacimiento().getNUI());
-            ps.setInt(3, arbitro.getEdad());
-            ps.setString(4, arbitro.getDireccion());
-            ps.setString(5, arbitro.getEmail());
-            ps.setInt(6, arbitro.getPartidosArbitrados());
+            ps.setString(1, equipo.getNombreEquipo());
+            ps.setString(2, equipo.getPaisOrigen());
             ps.executeUpdate();
-        } catch (Exception e) {
+        } catch (SQLException ex) {
+            Logger.getLogger(EquipoDB.class.getName()).log(Level.SEVERE, null, ex);
         }
+
     }
 
     @Override
@@ -42,18 +44,19 @@ public class ArbitroDB implements CRUD {
 
     @Override
     public void listar(String query) {
-        List<Arbitro> datos = new ArrayList<>();
+        List<ActaNacimiento> datos = new ArrayList<>();
         try {
             conector = Conexion.getConnection();
             ps = conector.prepareStatement(query);
             rs = ps.executeQuery();
 
             while (rs.next()) {
-                Arbitro arbitro = new Arbitro();
-                arbitro.getActaNacimiento().setNombres(rs.getString(2));
-                arbitro.getActaNacimiento().setApellidos(rs.getString(3));
-                arbitro.getActaNacimiento().setNacionalidad(rs.getString(4));
-                datos.add(arbitro);
+                ActaNacimiento acta = new ActaNacimiento();
+                acta.setNUI(rs.getString(1));
+                acta.setNombres(rs.getString(2));
+                acta.setApellidos(rs.getString(3));
+                acta.setNacionalidad(rs.getString(4));
+                datos.add(acta);
             }
         } catch (Exception e) {
         }
@@ -61,17 +64,16 @@ public class ArbitroDB implements CRUD {
 
     @Override
     public void modificar(String query, Object objeto) {
-        Arbitro arbitro = (Arbitro) objeto;
+        ActaNacimiento acta = (ActaNacimiento) objeto;
         try {
             conector = Conexion.getConnection();
             ps = conector.prepareStatement(query);
-            ps.setString(1, arbitro.getActaNacimiento().getNombres());
-            ps.setString(2, arbitro.getActaNacimiento().getApellidos());
-            ps.setString(3, arbitro.getActaNacimiento().getNacionalidad());
+            ps.setString(1, acta.getNUI());
+            ps.setString(2, acta.getNombres());
+            ps.setString(3, acta.getApellidos());
+            ps.setString(4, acta.getNacionalidad());
             ps.executeUpdate();
         } catch (Exception e) {
         }
     }
-    
-    
 }
